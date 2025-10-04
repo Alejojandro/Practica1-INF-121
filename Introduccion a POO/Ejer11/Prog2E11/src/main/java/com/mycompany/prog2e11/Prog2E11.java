0f1380e89eb39a216916ce84c79f99265995d5df
Alejojandro <yltraalejandro@gmail.com>
2025-10-04 19:28:56 -0400

    Delete Introduccion a POO directory

--- Introduccion a POO/Ejer11/Prog2E11/src/main/java/com/mycompany/prog2e11/Prog2E11.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.prog2e11;
-public class Prog2E11 {
-    public static void main(String[] args) {
-        Empleado e1=new Empleado("Juan","Varista");
-        Pedido p1=new Pedido("Cafe","En proceso");
-        p1.mostrar();
-        e1.mostrar();
-    }
-}
